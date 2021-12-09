7c06e8dae5284d072a2b7f4dd3ab5c305d617083
yaewonLee <yw990324@gmail.com>
2021-12-09 21:44:44 +0900

    boj: 개

---
+++ Boj_Charlotte/10172_charlotte.swift
@@ -0,0 +1,18 @@
+//
+//  10172_charlotte.swift
+//  Boj_Charlotte
+//
+//  Created by 이예원 on 2021/12/09.
+//
+
+import Foundation
+
+let dog = """
+|\\_/|
+|q p|   /}
+( 0 )\"\"\"\\
+|"^"`    |
+||_/=\\\\__|
+"""
+
+print(dog)
