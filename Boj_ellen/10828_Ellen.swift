75685cfdab974a45f209a7bc1d2f0c52f1515c2a
ellenJ <jcrescent61@gmail.com>
2021-12-10 02:40:37 +0900

    boj: 스택

---
+++ Boj_ellen/10828_Ellen.swift
@@ -0,0 +1,38 @@
+//
+//  10828_Ellen.swift
+//  Boj_Ellen
+//
+//  Created by Ellen on 2021/12/10.
+//
+
+let userInput = Int(readLine()!)!
+let params = ["push", "pop", "size", "empty", "top"]
+var stack: [Int] = []
+
+for _ in 1...userInput {
+    let userInput = readLine()!
+    let splitedInput = userInput.split(separator: " ")
+    if params.contains(String(splitedInput[0])) {
+        switch splitedInput[0] {
+        case "push":
+            stack.append(Int(splitedInput[1])!)
+        case "pop":
+            if stack.isEmpty == true {
+                print("-1")
+            } else {
+                print(stack.popLast()!)
+            }
+        case "size":
+            print(stack.count)
+        case "empty":
+            if stack.isEmpty == true {
+                print("1")
+            } else {
+                print("0")
+            }
+        case "top":
+            print(stack.last!)
+        default: break
+        }
+    }
+}
