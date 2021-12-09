4386f92b53a0b4045d4365edc84cca1526645f4f
ellenJ <jcrescent61@gmail.com>
2021-12-09 19:38:29 +0900

    boj: A/B

---
+++ Boj_ellen/1008_Ellen.swift
@@ -0,0 +1,10 @@
+//
+//  1008_Ellen.swift
+//  Boj_Ellen
+//
+//  Created by Ellen on 2021/12/09.
+//
+
+let userInput = readLine()!
+let array = userInput.split(separator: " ")
+print(Double(array[0])! / Double(array[1])!)
