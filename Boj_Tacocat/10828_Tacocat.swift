2512190350b128812b1c4b4b4f8c9d111728e039
Ldoy <leedoy7989@gmail.com>
2021-12-10 14:27:50 +0900

    feat: Add 10828 Boj problem

--- Boj_Tacocat/10828_Tacocat.swift
+++ Boj_Tacocat/10828_Tacocat.swift
@@ -6,14 +6,81 @@
 //
 
 import Foundation
-//
-//정수를 저장하는 스택을 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
-//
-//명령은 총 다섯 가지이다.
-//
-//push X: 정수 X를 스택에 넣는 연산이다.
-//pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-//size: 스택에 들어있는 정수의 개수를 출력한다.
-//empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
-//top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
 
+struct Stack {
+    static var store: [Int] = []
+    private static let somethingInStore = 1
+    private static let emptyStore = 0
+    private static let invalidRequest = -1
+    static var result: [Int] = []
+    
+    
+    static func push(this number: Int) {
+        self.store.append(number)
+    }
+    
+    static func pop() {
+       let last =  self.store.popLast()
+        result.append(last ?? .zero)
+    }
+    
+    static func size() {
+        result.append(self.store.count)
+    }
+    
+    static func empty() {
+        if self.store.count == .zero {
+            result.append(somethingInStore)
+        } else {
+            result.append(emptyStore)
+        }
+    }
+    
+    static func top() {
+        let last =  self.store.last
+        result.append(last ?? .zero)
+    }
+}
+
+func check(command: String) {
+    let commandDetail = command.components(separatedBy: " ")
+    
+    guard let first = commandDetail.first else {
+        return
+    }
+    
+    switch first {
+    case "push":
+        if let second = commandDetail.last,
+           let secondNumber = Int(second) {
+            Stack.push(this: secondNumber)
+        }
+    case "top":
+        Stack.top()
+    case "size":
+        Stack.size()
+    case "empty":
+        Stack.empty()
+    case "pop":
+        Stack.pop()
+    default:
+        print("Error")
+    }
+}
+
+func start() {
+    let firstInput = readLine()!
+    var loopNumber = Int(firstInput)!
+    
+    while loopNumber != .zero {
+        let input = readLine()!
+        check(command: input)
+        loopNumber -= 1
+    }
+    
+    Stack.result.map { number in
+        print(number)
+    }
+}
+
+start()
