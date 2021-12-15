ec232ffef23dcdc43c433ace840cc08ec5bd43a6
soll4u <dasollpark@gmail.com>
2021-12-16 02:57:48 +0900

    concepts: Stack 구현

---
+++ BasicConcepts/Stack_Soll.swift
@@ -0,0 +1,27 @@
+//
+//  Stack_Soll.swift
+//  BasicConcepts
+//
+//  Created by Dasoll Park on 2021/12/16.
+//
+
+class Stack<T> {
+    var stack: [T] = []
+    var top: T? {
+        stack.last
+    }
+    var isEmpty: Bool {
+        stack.isEmpty ? true : false
+    }
+    var size: Int {
+        stack.count
+    }
+    
+    func push(item: T) {
+        stack.append(item)
+    }
+    
+    func pop() -> T? {
+        stack.popLast()
+    }
+}
