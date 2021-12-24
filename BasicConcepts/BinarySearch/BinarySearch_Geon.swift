2d2a92264be8ed71de611042d7abab69b86431e3
jgkim1008 <rlawnsrjs100@naver.com>
2021-12-24 17:32:55 +0900

    concepts: 이진탐색

---
+++ BasicConcepts/BinarySearch/BinarySearch_Geon.swift
@@ -0,0 +1,18 @@
+//
+//  SequentialSearch_Geon .swift
+//  Excution
+//
+//  Created by 김준건 on 2021/12/24.
+//
+
+func search<T: BinaryInteger>(into list: [T], target: T, startIndex: Int, endIndex: Int) -> Int {
+    let middle = startIndex + endIndex / 2
+    if list[startIndex] == target {
+        return startIndex
+    }
+    else if middle > target {
+        return search(into: list, target: target, startIndex: startIndex + 1, endIndex: middle)
+    } else {
+        return search(into: list, target: target, startIndex: middle, endIndex: endIndex)
+    }
+}
